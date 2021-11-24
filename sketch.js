7ba601c5811de9723a9d64351afa8c7891f08159
Seany-mibb <86206802+Seany-mibb@users.noreply.github.com>
2021-11-23 16:48:54 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function transform()
+{
+    document.getElementById("noodles").style.color = "yellow";
+}
+
+function activate()
+{
+    document.getElementById("about").style.display = "inline-block";
+}
+
+function deactivate()
+{
+    document.getElementById("about").style.display = "none";
+}
